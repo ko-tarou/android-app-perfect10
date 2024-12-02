6e775cd8509f71ea3a0e95ababfa8306a2027e06
ko-tarou <akokoa1221@gmail.com>
2024-12-02 12:59:26 +0900

    ナビゲーション

--- app/src/main/java/com/websarva/wings/android/perfect10/ui/TopPage.kt
+++ app/src/main/java/com/websarva/wings/android/perfect10/ui/TopPage.kt
@@ -2,10 +2,13 @@ package com.websarva.wings.android.perfect10.ui
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
 
@@ -16,6 +19,14 @@ fun TopPage(navController: NavController){
             .fillMaxSize()
     ){
         Text(text = "top")
+
+        Button(
+            onClick = {navController.navigate("gamepage")},
+            modifier = Modifier
+                .padding(16.dp)
+        ) {
+            Text(text = "はじめる")
+        }
     }
 }
 
